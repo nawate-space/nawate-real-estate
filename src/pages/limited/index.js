dfa06cd2f79e00a0ea06629882724adf89290429
seetake <see.take.swamp@gmail.com>
2020-06-04 09:59:02 +0900

    限定コンテンツページ追加

---
+++ src/pages/limited/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { kebabCase } from "lodash";
+import { Helmet } from "react-helmet";
+import { Link, graphql } from "gatsby";
+import Layout from "../../components/Layout";
+
+const LimitedPage = () => (
+  <Layout>
+    <section className="section">
+      <Helmet title="限定コンテンツ" />
+      <div className="container content">
+        <div className="columns">
+          <div
+            className="column is-10 is-offset-1"
+            style={{ marginBottom: "6rem" }}
+          >
+            <h1 className="title is-size-2 is-bold-light">限定コンテンツ</h1>
+          </div>
+        </div>
+      </div>
+    </section>
+  </Layout>
+);
+
+export default LimitedPage;
