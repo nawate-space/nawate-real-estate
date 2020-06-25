96cd5006b63ac267163d6be157b6ff4f80b68270
nariryo <nr1995amg@gmail.com>
2020-06-25 18:03:24 +0900

    限定コンテンツ改修

--- src/pages/limited/index.js
+++ src/pages/limited/index.js
@@ -35,7 +35,7 @@ const LimitedPage = () => (
           width="100%"
           height="300"
           scrolling="no"
-          frameBorder="no"
+          frameborder="no"
           allow="autoplay"
           src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/844161601&color=%2382d1c7&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
         ></iframe>
